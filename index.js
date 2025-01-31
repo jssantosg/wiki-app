3c59b512e998606f2bec302fd55881e18d5edbcf
Johan Santos <jssantosg@gmail.com>
2025-01-31 13:51:21 -0800

    Edit post functionality added

--- index.js
+++ index.js
@@ -25,12 +25,22 @@ app.get('/article/:articlename', (req, res) => {
 
 app.get('/article/:articlename/edit', (req, res) => {
     try {
-        const article = db.article_getByEncodedName(req.params.articlename);
-        res.render('edit', { article });
+      const article = db.article_getByEncodedName(req.params.articlename);
+      res.render('edit', { article });
     } catch (error) {
-        res.status(404).send('Article not found');
+      res.status(404).send('Article not found');
     }
-});
+  });
+  
+  app.post('/article/:articlename/edit', (req, res) => {
+    try {
+      const { contents } = req.body;
+      db.article_editByEncodedName(req.params.articlename, contents);
+      res.redirect(`/article/${req.params.articlename}`);
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  });
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
