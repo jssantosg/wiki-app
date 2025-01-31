b2bd4bee27f3a2555fd3ad9eef8a5e64f619fb15
Johan Santos <jssantosg@gmail.com>
2025-01-31 14:27:35 -0800

    Create new article link show a form to create an article now

--- index.js
+++ index.js
@@ -62,12 +62,8 @@ app.post('/article/:articlename/delete', (req, res) => {
 
 app.get('/newarticle', (req, res) => {
     res.render('newarticle');
-  });
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 });
-
-
-
-
